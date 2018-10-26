2f0e9c3d3c15e3af64cda8f53dc7d1f2f65ec34d
Diego G.G <dieinmort@gmail.com>
2018-10-26 10:20:03 -0500

    mergesort serial

--- src/main/java/sorts/MergeSort.java
+++ src/main/java/sorts/MergeSort.java
@@ -4,6 +4,7 @@ public class MergeSort implements Sortable {
 
 	@Override
 	public int[] sort(int[] in) {
+		topDownMergeSort(in, in.clone(), in.length);
 		return in;
 	}
 
@@ -19,4 +20,36 @@ public class MergeSort implements Sortable {
 		return "MergeSort";
 	}
 
+	private void topDownMergeSort(int[] A, int[] B, int n) {
+		copyArray(A, 0, n, B);
+		topDownSplitMerge(B, 0, n, A);
+	}
+
+	private void topDownSplitMerge(int[] B, int iBegin, int iEnd, int[] A) {
+		if (iEnd - iBegin < 2) {
+			return;
+		}
+		int iMiddle = (iEnd + iBegin) / 2;
+		topDownSplitMerge(A, iBegin, iMiddle, B);
+		topDownSplitMerge(A, iMiddle, iEnd, B);
+		topDownMerge(B, iBegin, iMiddle, iEnd, A);
+	}
+
+	private void topDownMerge(int[] A, int iBegin, int iMiddle, int iEnd, int[] B) {
+		int i = iBegin, j = iMiddle;
+
+		for (int k = iBegin; k < iEnd; ++k) {
+			if (i < iMiddle && (j >= iEnd || A[i] <= A[j])) {
+				B[k] = A[i];
+				++i;
+			} else {
+				B[k] = A[j];
+				++j;
+			}
+		}
+	}
+
+	private void copyArray(int A[], int iBegin, int iEnd, int B[]) {
+		System.arraycopy(A, iBegin, B, iBegin, iEnd - iBegin);
+	}
 }
